e2d7e427a240223d448df831e47465ea0eb505c5
Sebastian Podgajny <sebastian.podgajny@recruitee.com>
2023-04-13 15:19:44 +0200

    feat(cdk): keep only cdk

--- src/components-examples/material/datepicker/datepicker-actions/datepicker-actions-example.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-/** @title Datepicker action buttons */
-@Component({
-  selector: 'datepicker-actions-example',
-  templateUrl: 'datepicker-actions-example.html',
-  styleUrls: ['datepicker-actions-example.css'],
-})
-export class DatepickerActionsExample {}
--- src/e2e-app/main.ts
+++
@@ -1,8 +0,0 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowser} from '@angular/platform-browser';
-
-import {MainModule} from './main-module';
-
-enableProdMode();
-
-platformBrowser().bootstrapModule(MainModule);
