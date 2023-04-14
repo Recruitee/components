702601f24e85bacee57450df4b0637f2699037a0
Sebastian Podgajny <sebastian.podgajny@recruitee.com>
2023-04-14 15:35:37 +0200

    feat(cdk): add keep-cdk script

--- keep-cdk.sh
+++ keep-cdk.sh
@@ -3,3 +3,4 @@ shopt -s dotglob
 rm -rf !("keep-cdk.sh"|"src"|".git"|".idea")
 rm -rf src/!("cdk"|"cdk-experimental")
 rm -rf src/cdk/*("testing"|"schematics")
+rm src/*("cdk"|"cdk-experimental")/*("BUILD.bazel"|"config.bzl"|"index.ts"|"package.json"|"tsconfig.json")
